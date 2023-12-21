074e5e9719b7592c49ab6b9de066a2cda2516424
nur arifin <nurrifin65@gmail.com>
2023-12-21 10:04:38 +0700

    Initial commit

---
+++ app/src/main/java/org/tensorflow/lite/examples/gestureclassification/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package org.tensorflow.lite.examples.gestureclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
